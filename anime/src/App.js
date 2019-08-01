94a5afac8e22d5914be082c0efef7bba311bac6d
Laule <zhzy95@outlook.com>
2019-08-01 15:51:35 +0800

    anime-over

--- anime/src/App.js
+++ anime/src/App.js
@@ -1,19 +1,43 @@
 import React, {Component, Fragment} from 'react';
+import './style.css';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            // show: true
+            list: []
         }
         this.handleToggle = this.handleToggle.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
     }
 
     render() {
         return (
             <Fragment>
-                <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-                <button onClick={this.handleToggle}>toggle</button>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <CSSTransition key={index}
+                                    timeout={1000}
+                                    classNames='fade'
+                                    unmountOnExit
+                                    onEntered={(el) => {
+                                        el.style.color = 'blue'
+                                    }}
+                                    appear={true}
+                                >
+                                    <div>{item}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+
+                </TransitionGroup>
+                <button onClick={this.handleAddItem}>toggle</button>
             </Fragment>
         )
     }
@@ -23,6 +47,15 @@ class App extends Component {
             show: this.state.show ? false : true
         })
     }
+
+    handleAddItem() {
+        this.setState((prevState) => {
+            return {
+                list: [...prevState.list, 'item']
+            }
+        })
+    }
 }
 
 export default App;
+
